1ef786545a5bb9fc262c10854df45f5c3a052daf
Bogdan Maxim <bogdan.maxim@geminicad.com>
2019-11-14 22:14:29 +0200

    androidX refactor

---
+++ mirror/src/main/java/com/github/slashmax/aamirror/AppListAdapter.java
@@ -0,0 +1,50 @@
+package com.github.slashmax.aamirror;
+
+import android.content.Context;
+import androidx.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class AppListAdapter extends ArrayAdapter<AppEntry>
+{
+    private final LayoutInflater m_Inflater;
+
+    AppListAdapter(Context context)
+    {
+        super(context, android.R.layout.simple_list_item_2);
+        m_Inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void setData(List<AppEntry> data)
+    {
+        clear();
+        if (data != null)
+            addAll(data);
+    }
+
+    @Override
+    public @NonNull View getView(int position, View convertView, @NonNull ViewGroup parent)
+    {
+        View view;
+
+        if (convertView == null)
+            view = m_Inflater.inflate(R.layout.list_item_icon_text, parent, false);
+        else
+            view = convertView;
+
+        AppEntry item = getItem(position);
+        if (item != null)
+        {
+            ((ImageView) view.findViewById(R.id.list_item_icon)).setImageDrawable(item.getIcon());
+            ((TextView) view.findViewById(R.id.list_item_text)).setText(item.getLabel());
+        }
+
+        return view;
+    }
+}
---
+++ mirror/src/main/java/com/github/slashmax/aamirror/CarFragment.java
@@ -0,0 +1,30 @@
+package com.github.slashmax.aamirror;
+
+import androidx.fragment.app.Fragment;
+import android.util.Log;
+
+import com.google.android.apps.auto.sdk.CarActivity;
+
+public class CarFragment extends Fragment
+{
+    private static final String TAG = "CarFragment";
+
+    private CarActivity m_Activity;
+
+    public CarFragment() {
+        super();
+        Log.d(TAG, "CarFragment");
+    }
+
+    public void SetActivity(CarActivity activity)
+    {
+        Log.d(TAG, "SetActivity");
+        m_Activity = activity;
+    }
+
+    public CarActivity GetActivity()
+    {
+        Log.d(TAG, "GetActivity");
+        return m_Activity;
+    }
+}
